29c76bb07394ad59c4264ea40567a5d331d76165
Elias Lundell <elilun03@gmail.com>
2024-11-01 21:39:59 +0100

    add watchdog and expand dockercontainer

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,18 @@
-FROM ubuntu:20.04
+FROM ubuntu:22.04
 
 RUN apt-get update
 
 RUN apt-get install -y python3
+RUN pip3 install watchdog
+
+RUN deb [arch=amd64 signed-by=/usr/share/keyrings/gpg-pub-moritzbunkus.gpg] https://mkvtoolnix.download/ubuntu/ jammy main
+RUN deb-src [arch=amd64 signed-by=/usr/share/keyrings/gpg-pub-moritzbunkus.gpg] https://mkvtoolnix.download/ubuntu/ jammy main
+
+RUN apt-get update
+RUN apt-get install mkvtoolnix
 
 RUN mkdir -p /home/src_extractor
+RUN mkdir -p /home/watching
 
 WORKDIR /home/src_extractor
 
--- main.py
+++ main.py
@@ -1,1 +1,25 @@
-print("Hello")
+import time
+import watchdog
+
+from os import path
+from watchdog.events import FileSystemEvent, FileSystemEventHandler
+from watchdog.observers import Observer
+
+dirname = path.dirname(__file__)
+watching = path.join(dirname, "..", "watching")
+
+class MyEventHandler(FileSystemEventHandler):
+    def on_any_event(self, event: FileSystemEvent) -> None:
+        print(event)
+
+event_handler = MyEventHandler()
+observer = Observer()
+observer.schedule(event_handler, watching, recursive=True)
+observer.start()
+
+try:
+    while True:
+        time.sleep(1)
+finally:
+    observer.stop()
+    observer.join()
