2aad867fdca5bb95961d2ab48fcb41cd747e0131
Elias Lundell <elilun03@gmail.com>
2024-11-01 22:04:48 +0100

    wactch for file modification events

--- main.py
+++ main.py
@@ -8,9 +8,19 @@ from watchdog.observers import Observer
 dirname = path.dirname(__file__)
 watching = path.join(dirname, "..", "watching")
 
+active_files = {}
+
 class MyEventHandler(FileSystemEventHandler):
     def on_any_event(self, event: FileSystemEvent) -> None:
-        print(event)
+        srcpath = event.src_path
+        if type(srcpath) is not str:
+            srcpath = srcpath.decode("utf-8") 
+
+        if not event.src_path.endswith(".mkv"):
+            return
+
+        if event.event_type == "modified":
+            active_files[srcpath] = time.time()
 
 event_handler = MyEventHandler()
 observer = Observer()
@@ -20,6 +30,10 @@ observer.start()
 try:
     while True:
         time.sleep(1)
+        for file in active_files:
+            if time.time() - active_files[file] > 10:
+                del active_files[file]
+                print(file)
 finally:
     observer.stop()
     observer.join()
